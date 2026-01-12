3d8b4e0a8bfe1a222fbb335c04708c2ba86b7bd4
Sergi Mallén <121065349+MallenK@users.noreply.github.com>
2026-01-12 11:14:03 +0000

    test: force minimal vercel function response

---
+++ api/chat.js
@@ -0,0 +1,7 @@
+export const config = { runtime: "nodejs" };
+
+export default function handler(req, res) {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify({ ok: true }));
+}
